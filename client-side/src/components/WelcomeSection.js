60f0705b9f7fcefe4b24b4d05c8600e9f9d7ffbb
Putumani <putumaninomzaza@gmail.com>
2024-05-07 12:13:14 +0200

    deploying

--- client-side/src/components/WelcomeSection.js
+++ client-side/src/components/WelcomeSection.js
@@ -5,7 +5,7 @@ import "./WelcomeSection.css";
 const WelcomeSection = () => {
   return (
     <section className="welcome-section">
-      <div className="container margin_20_20spacing-bottom">
+      <div className="container margin_20_20 spacing-bottom">
         <h1 className="main_title text-danger d-block font-slim">
           <em></em>
           Welcome to OTJ Pride GuestHouse
@@ -13,7 +13,7 @@ const WelcomeSection = () => {
         <span className="d-block text-secondary increased-font-size font-slim">
           Guest House and Bed & Breakfast
         </span>
-        <div className="slim-bar bg-danger"></div>
+        <div class="slim-bar bg-danger"></div>
       </div>
       <div className="row mt-4">
         <div className="col-md-6">
@@ -34,7 +34,7 @@ const WelcomeSection = () => {
           <p className="gray-paragraph">
             OTJ Pride Guest House features room service. In addition, as a
             valued OTJ Pride Guest House guest, you can enjoy a pool and an
-            on-site restaurant that are available on-site. Guests arriving by
+            on-site restaurant that is available on-site. Guests arriving by
             vehicle have access to free parking.
           </p>
           <p className="gray-paragraph">
